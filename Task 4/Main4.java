6447c0653ea2c6642c5764c040511f8fd62fbbbf
tahsinahmed01 <140525722+tahsinahmed01@users.noreply.github.com>
2026-06-23 11:43:15 +0600

    Implement message sending via Email and SMS

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,39 @@
+interface MessageService {
+    void sendMessage(String message);
+}
 
+class EmailSender implements MessageService {
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+class SMSSender implements MessageService {
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+class NotificationService {
+    private MessageService messageService;
+
+    public NotificationService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    public void alertUser(String msg) {
+        messageService.sendMessage(msg);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MessageService email = new EmailSender();
+        NotificationService service1 = new NotificationService(email);
+        service1.alertUser("Hello via Email");
+
+        MessageService sms = new SMSSender();
+        NotificationService service2 = new NotificationService(sms);
+        service2.alertUser("Hello via SMS");
+    }
+}
