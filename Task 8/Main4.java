cb24f05a9bcd91e57c762173720b2b19c8639047
tahsinahmed01 <140525722+tahsinahmed01@users.noreply.github.com>
2026-07-21 11:29:03 +0600

    Implement singleton pattern in ConfigurationManager

--- Task 8/Main4.java
+++ Task 8/Main4.java
@@ -1,1 +1,27 @@
+ class ConfigurationManager {
+    private static volatile ConfigurationManager instance;
 
+    private ConfigurationManager(){
+        System.out.println("Reading heavy config file from disk... (Only happens once)");
+
+    }
+    public static ConfigurationManager getINSTANCE(){
+        if(instance == null){
+            synchronized(ConfigurationManager.class){
+                if(instance == null){
+                    instance = new ConfigurationManager();
+                }
+            }
+        }
+        return instance;
+    }
+}
+public class Main1{
+    public static void main(String[] args) {
+        ConfigurationManager config1 = ConfigurationManager.getINSTANCE();
+        ConfigurationManager config2 = ConfigurationManager.getINSTANCE();
+
+        System.out.println("Same instance in MEMORY? " + (config1 == config2));
+    }
+
+}
